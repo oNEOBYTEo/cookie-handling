8327ec76999bc9c68b477748520f0021d7abddd1
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-10-13 18:45:53 -0500

    Layout created with navbar

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import { FC, PropsWithChildren } from 'react';
+import Head from 'next/head';
+import { Navbar } from '../ui';
+
+export const Layout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      {/* <Head></Head> */}
+      <nav>
+        <Navbar />
+      </nav>
+
+      <main style={{ padding: '20px 50px' }}>{children}</main>
+    </>
+  );
+};
